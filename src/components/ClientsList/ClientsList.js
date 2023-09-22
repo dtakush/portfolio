0e485378fe71563a0f9928d750859183d7eacd7c
Daria Takush <dashatakush@yandex.ru>
2023-09-22 17:30:51 +0300

    New clients

--- src/components/ClientsList/ClientsList.js
+++ src/components/ClientsList/ClientsList.js
@@ -13,13 +13,16 @@ function ClientsList({onEmptyClientClick}) {
                 <NavLink to='#' className='clients-list_text' onClick={onEmptyClientClick}>Роснефть</NavLink>
                 <NavLink to='/work/remit-video' className='clients-list_text'>Ремит</NavLink>
                 <NavLink to='/work/dom-22' className='clients-list_text'>Донстрой</NavLink>
+                <NavLink to='/work/dolfin' className='clients-list_text'>Alvogen</NavLink>
                 <NavLink to='#' className='clients-list_text' onClick={onEmptyClientClick}>БНА “Траст”</NavLink>
+                <NavLink to='/work/familia-video' className='clients-list_text'>Familia</NavLink>
+                <NavLink to='/work/desrinit' className='clients-list_text'>Teva</NavLink>
                 <NavLink to='/work/mayak-video' className='clients-list_text'>Маяк</NavLink>
                 <NavLink to='/work/mobiado-outdoor' className='clients-list_text'>Mobiado</NavLink>
+                <NavLink to='/work/novo-nordisk' className='clients-list_text'>Novo Nordisk</NavLink>
                 <NavLink to='#' className='clients-list_text' onClick={onEmptyClientClick}>Roche</NavLink>
                 <NavLink to='/work/fungoterbin' className='clients-list_text'>Stada</NavLink>
                 <NavLink to='/work/boehringer-ingelheim' className='clients-list_text'>Boehringer Ingelheim</NavLink>
-                <NavLink to='/work/novo-nordisk' className='clients-list_text'>Novo Nordisk</NavLink>
                 <NavLink to='#' className='clients-list_text' onClick={onEmptyClientClick}>Amigo</NavLink>
             </div>
         </>    
