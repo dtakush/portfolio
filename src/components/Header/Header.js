134b8da4e0226fd031c7b0e72f20169220a8f526
dtakush <dashatakush@yandex.ru>
2023-10-09 17:23:09 +0300

    New Texts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -17,7 +17,7 @@ function Header() {
                         <div className="header__links-small">
                             <NavLink to="/about" className="header__link header__link-small">Кто я?</NavLink>
                             <NavLink to="/work" className="header__link header__link-small">Работы.</NavLink>
-                            <NavLink to="/clients" className="header__link header__link-small header__link-small_clients">С кем работала?</NavLink>
+                            <NavLink to="/clients" className="header__link header__link-small header__link-small_clients">Клиенты.</NavLink>
                             <NavLink to="/contact" className="header__link header__link-small">Контакты.</NavLink>
                         </div>
                     </header>)
@@ -28,7 +28,7 @@ function Header() {
                         <div className="header__links-main">
                             <NavLink to="/about" className="header__link header__link-main">Кто я?</NavLink>
                             <NavLink to="/work" className="header__link header__link-main">Работы.</NavLink>
-                            <NavLink to="/clients" className="header__link header__link-main">С кем работала?</NavLink>
+                            <NavLink to="/clients" className="header__link header__link-main">Клиенты.</NavLink>
                             <NavLink to="/contact" className="header__link header__link-main">Контакты.</NavLink>
                         </div>
                         
