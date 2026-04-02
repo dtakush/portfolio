09e2edadda1c9bf0c950634caabd680ff04f30bd
dtakush <dashatakush@yandex.ru>
2026-04-02 21:31:22 +0300

    logo_update

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 //import { useMediaQuery } from "react-responsive";
 
-import logo from '../../../public/main_images/LOGO.svg';
+import logo from '../../pics/LOGO.svg';
 
 function Header() {
     const location = useLocation();
